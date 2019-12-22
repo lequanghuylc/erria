701c1796377d59a7c05fb32ba58d646ba7bca24b
Huy Le <lequanghuylc@gmail.com>
2019-12-22 10:43:38 +0700

    change the way detect async function

--- decorator.js
+++ decorator.js
@@ -5,30 +5,44 @@ function erria(target, propertyName, propertyDesciptor) {
     if (typeof target[propertyName] !== 'function')
         return;
     const method = propertyDesciptor.value;
-    if (isAsync_1.isAsync(method)) {
-        propertyDesciptor.value = async function (...args) {
-            try {
-                let result = await method.apply(this, args);
-                if (isAsync_1.isPromise(result))
-                    result = await result;
-                return [result, null];
+    propertyDesciptor.value = function (...args) {
+        try {
+            const initialExec = method.apply(this, args);
+            if (isAsync_1.isPromise(initialExec)) {
+                return new Promise((resolve) => {
+                    initialExec
+                        .then(res => resolve([res, null]))
+                        .catch(err => resolve([null, err]));
+                });
             }
-            catch (err) {
-                return [null, err];
+            else {
+                return [initialExec, null];
             }
-        };
-    }
-    else {
-        propertyDesciptor.value = function (...args) {
-            try {
-                const result = method.apply(this, args);
-                return [result, null];
-            }
-            catch (err) {
-                return [null, err];
-            }
-        };
-    }
+        }
+        catch (err) {
+            return [null, err];
+        }
+    };
+    // if (isAsync(method)) {
+    //   propertyDesciptor.value = async function (...args: any[]) {
+    //     try {
+    //       let result = await method.apply(this, args);
+    //       if (isPromise(result)) result = await result;
+    //       return [result, null];
+    //     } catch(err) {
+    //       return [null, err]
+    //     }
+    //   }
+    // } else {
+    //   propertyDesciptor.value = function (...args: any[]) {
+    //     try {
+    //       const result = method.apply(this, args);
+    //       return [result, null];
+    //     } catch(err) {
+    //       return [null, err]
+    //     }
+    //   }
+    // }
     return propertyDesciptor;
 }
 exports.default = erria;
--- decorator.ts
+++ decorator.ts
@@ -8,26 +8,43 @@ export default function erria(
   if (typeof target[propertyName] !== 'function') return;
   const method = propertyDesciptor.value;
 
-  if (isAsync(method)) {
-    propertyDesciptor.value = async function (...args: any[]) {
-      try {
-        let result = await method.apply(this, args);
-        if (isPromise(result)) result = await result;
-        return [result, null];
-      } catch(err) {
-        return [null, err]
+  propertyDesciptor.value = function (...args: any[]) {
+    try {
+      const initialExec = method.apply(this, args);
+      if (isPromise(initialExec)) {
+        return new Promise((resolve) => {
+          initialExec
+          .then(res => resolve([res, null]))
+          .catch(err => resolve([null, err]));
+        })
+      } else {
+        return [initialExec, null];
       }
-    }
-  } else {
-    propertyDesciptor.value = function (...args: any[]) {
-      try {
-        const result = method.apply(this, args);
-        return [result, null];
-      } catch(err) {
-        return [null, err]
-      }
+    } catch(err) {
+      return [null, err];
     }
   }
+
+  // if (isAsync(method)) {
+  //   propertyDesciptor.value = async function (...args: any[]) {
+  //     try {
+  //       let result = await method.apply(this, args);
+  //       if (isPromise(result)) result = await result;
+  //       return [result, null];
+  //     } catch(err) {
+  //       return [null, err]
+  //     }
+  //   }
+  // } else {
+  //   propertyDesciptor.value = function (...args: any[]) {
+  //     try {
+  //       const result = method.apply(this, args);
+  //       return [result, null];
+  //     } catch(err) {
+  //       return [null, err]
+  //     }
+  //   }
+  // }
 
   return propertyDesciptor;
 };
